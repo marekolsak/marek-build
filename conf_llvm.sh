ec53aa3e5129bec3018474969abef3aec7c67a89
Marek Olšák <marek.olsak@amd.com>
2018-05-01 19:54:47 -0400

    enable ccache for LLVM

--- conf_llvm.sh
+++ conf_llvm.sh
@@ -7,7 +7,8 @@ cmake .. -G Ninja -DCMAKE_INSTALL_PREFIX=/usr/llvm/x86_64-linux-gnu -DLLVM_TARGE
                   -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON \
                   -DLLVM_APPEND_VC_REV=OFF -DCMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO="-fuse-ld=gold" \
                   -DCMAKE_C_FLAGS_RELWITHDEBINFO="-O2 -g -fno-omit-frame-pointer" \
-                  -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-O2 -g -fno-omit-frame-pointer" 
+                  -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-O2 -g -fno-omit-frame-pointer" \
+                  -DLLVM_CCACHE_BUILD=ON
 cd ..
 
 mkdir -p build32
@@ -17,4 +18,4 @@ cmake .. -G Ninja -DCMAKE_INSTALL_PREFIX=/usr/llvm/i386-linux-gnu -DLLVM_TARGETS
                   -DLLVM_APPEND_VC_REV=OFF -DCMAKE_SHARED_LINKER_FLAGS_RELWITHDEBINFO="-fuse-ld=gold" \
                   -DCMAKE_C_FLAGS_RELWITHDEBINFO="-O2 -g -fno-omit-frame-pointer" \
                   -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-O2 -g -fno-omit-frame-pointer" \
-                  -DLLVM_BUILD_32_BITS=ON
+                  -DLLVM_BUILD_32_BITS=ON -DLLVM_CCACHE_BUILD=ON
