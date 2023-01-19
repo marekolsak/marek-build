f9d80772a18584c881583cbe3477808de050b2a5
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2023-01-19 11:39:28 +0100

    Add PREFIX support in conf_drm.sh

--- conf_drm.sh
+++ conf_drm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+prefix=${PREFIX:-/usr}
+
 if test x$1 = x32; then
     is64bit=disabled
     archdir=i386-linux-gnu
@@ -14,7 +16,7 @@ rm -r build$1
 
 cflags="-fno-omit-frame-pointer"
 
-meson build$1 --prefix /usr --libdir /usr/lib/$archdir --buildtype debugoptimized \
-	-Dc_args=$cflags -Dc_link_args=$cflags -Dpkg_config_path=/usr/lib/$archdir/pkgconfig \
+meson build$1 --prefix $prefix --libdir $prefix/lib/$archdir --buildtype debugoptimized \
+	-Dc_args=$cflags -Dc_link_args=$cflags -Dpkg_config_path=$prefix/lib/$archdir/pkgconfig \
 	-Detnaviv=enabled -Dexynos=enabled -Dfreedreno=enabled -Domap=enabled -Dtegra=enabled -Dvc4=enabled \
 	-Dcairo-tests=$is64bit
